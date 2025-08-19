551c8735df00fb4b1d869eb01ac5fbe709aa003b
Full Stack Dev(Web &  Mobile, Blockchain) <157616488+SrDev36428@users.noreply.github.com>
2025-08-19 13:33:47 -0700

    vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +9,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': './src',
     },
   },
 })
